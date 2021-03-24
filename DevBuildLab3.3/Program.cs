6564151837e5326ecef78aec896dfc014fba6e18
Kutub Talukder <kutubtalukder@quickenloans.com>
2021-03-24 19:14:22 -0400

    second commit

---
+++ DevBuildLab3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevBuildLab3._3
+{
+    class Program
+    {
+     
+        public static string reverseMethod(string userinput)
+        {
+            string revWord = "";
+            Stack<char> words = new Stack<char>();
+
+            foreach (char item in userinput)
+            {
+                words.Push(item);
+            }
+
+            foreach (char item in userinput)
+            {
+                revWord = revWord + words.Pop();
+            }
+            return revWord;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            //request user input
+            Console.WriteLine("Please Enter a word to reverse ");
+            string userInput = Console.ReadLine();
+
+            Console.Write(reverseMethod(userInput));
+
+
+
+        }
+    }
+}
